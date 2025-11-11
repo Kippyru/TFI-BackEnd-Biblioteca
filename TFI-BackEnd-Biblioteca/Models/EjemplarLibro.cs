ceafbea75732ba38de51550a2e31a01a9b3ffc01
F.Kevin <kfornier@gmail.com>
2025-11-10 21:48:26 -0300

    Revert "Configuracion de Api y Endpoint"

---
+++ TFI-BackEnd-Biblioteca/Models/EjemplarLibro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TFI_BackEnd_Biblioteca.Models;
+
+public partial class EjemplarLibro
+{
+    public string IdCodigoInventario { get; set; } = null!;
+
+    public string? CodigoUbicacion { get; set; }
+
+    public DateOnly? FechaAlta { get; set; }
+
+    public DateOnly? FechaBaja { get; set; }
+
+    public string? Isbn { get; set; }
+
+    public int? IdPrestamo { get; set; }
+
+    public virtual Prestamo? IdPrestamoNavigation { get; set; }
+
+    public virtual Libro? IsbnNavigation { get; set; }
+}
--- TFI-BackEnd-Biblioteca/Program.cs
+++ TFI-BackEnd-Biblioteca/Program.cs
@@ -1,23 +1,56 @@
+using Microsoft.EntityFrameworkCore;
+using TFI_BackEnd_Biblioteca.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Este codigo le "enseña"la aplicación cómo usar BibliotecaContext y de dónde sacar la cadena de conexión.
+// 1. Obtener la cadena de conexión que escribiste en appsettings.json
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// 2. Registrar el DbContext (BibliotecaContext) en el sistema.
+//    Esto se llama "Inyección de Dependencias".
+
+builder.Services.AddDbContext<BibliotecaContext>(options =>
+    options.UseSqlServer(connectionString));
+// --- FIN DE LA CONFIGURACIÓN DE EF ---
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddOpenApi();
+
+
+// Configuración de CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:4200") // ¡La URL de tu app Angular!
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
+// Aquí le decimos a la app que use la política que definimos
+app.UseCors("AllowAngularApp");
+
+// La línea de CORS debe ir ANTES de UseAuthorization
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
